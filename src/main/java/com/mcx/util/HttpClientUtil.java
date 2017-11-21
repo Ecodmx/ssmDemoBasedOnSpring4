fc9dad7952d1107cace969297fa3f134fd233164
Ecodmx <mycarrerx@163.com>
2017-11-22 00:57:01 +0800

    test httpclient getWhetherfromUrl String(xml) to Json gzip to string (messy code) sovled

---
+++ src/main/java/com/mcx/util/HttpClientUtil.java
@@ -0,0 +1,81 @@
+package com.mcx.util;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.GzipDecompressingEntity;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import com.mcx.usr.controller.SSLClient;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+import net.sf.json.xml.XMLSerializer;
+/*
+ * 利用HttpClient进行post请求的工具类
+ */
+public class HttpClientUtil {
+	public String doPost(String url,Map<String,String> map,String charset){
+		HttpClient httpClient = null;
+		HttpPost httpPost = null;
+		String result = null;
+		try{
+			httpClient = new SSLClient();
+			httpPost = new HttpPost(url);
+			//设置参数
+			List<NameValuePair> list = new ArrayList<NameValuePair>();
+			Iterator iterator = map.entrySet().iterator();
+			while(iterator.hasNext()){
+				Entry<String,String> elem = (Entry<String, String>) iterator.next();
+				list.add(new BasicNameValuePair(elem.getKey(),elem.getValue()));
+			}
+			if(list.size() > 0){
+				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list,charset);
+				httpPost.setEntity(entity);
+			}
+			HttpResponse response = httpClient.execute(httpPost);
+			Header[] headers = response.getHeaders("Content-Encoding");
+			System.out.println(response.getLastHeader("Content-Encoding"));
+			
+			if(response != null){
+				
+				response.setEntity(  new GzipDecompressingEntity(response.getEntity()));  
+				HttpEntity resEntity = response.getEntity();
+				if(resEntity != null){
+					
+					result = EntityUtils.toString(resEntity,charset);
+				}
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+	    String url = "http://wthrcdn.etouch.cn/WeatherApi?city=%E5%8C%97%E4%BA%AC";  
+	    String charset = "utf-8";  
+	    HttpClientUtil httpClientUtil = new HttpClientUtil();  
+	    Map<String,String> createMap = new HashMap<String,String>();  
+	    String httpOrgCreateTestRtn = httpClientUtil.doPost(url,createMap,charset);
+	    XMLSerializer xmlSerializer = new XMLSerializer();
+	    xmlSerializer.setSkipWhitespace(false);
+	    JSON jsonObj = xmlSerializer.read(httpOrgCreateTestRtn);
+	   
+        System.out.println("result:"+httpOrgCreateTestRtn);  
+        System.out.println(jsonObj.toString());
+	    
+	}
+}
